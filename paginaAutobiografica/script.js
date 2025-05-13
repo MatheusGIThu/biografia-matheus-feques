af46ae108e8833c1673ca550dafa9de3efc49ee8
Matheus <129387840+MatheusGIThu@users.noreply.github.com>
2025-05-13 19:29:57 -0300

    Delete paginaAutobiografica directory

--- paginaAutobiografica/script.js
+++
@@ -1,4 +0,0 @@
-function mensagem() {
-    const mensagemElemento = document.getElementById("mensagem");
-    mensagemElemento.textContent = "Quem possui a faculdade de ver a beleza, não envelhece - Franz Kafka";
-}

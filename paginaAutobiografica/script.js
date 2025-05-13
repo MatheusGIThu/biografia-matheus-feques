2a2d346adca64cd7a03e08e9b2977a935211c024
Matheus <129387840+MatheusGIThu@users.noreply.github.com>
2025-05-13 19:25:20 -0300

    Add files via upload

---
+++ paginaAutobiografica/script.js
@@ -0,0 +1,4 @@
+function mensagem() {
+    const mensagemElemento = document.getElementById("mensagem");
+    mensagemElemento.textContent = "Quem possui a faculdade de ver a beleza, não envelhece - Franz Kafka";
+}
